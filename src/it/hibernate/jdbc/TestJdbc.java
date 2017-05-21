2c89b6890914354a45bf0b384691d31d011ee801
sandro conte <sandroconte@MacBook-Pro-di-sandro.local>
2017-05-21 19:50:45 +0200

    test connection jdbc

---
+++ src/it/hibernate/jdbc/TestJdbc.java
@@ -0,0 +1,23 @@
+package it.hibernate.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://127.0.0.1:3306/hibernate?useSSL=false";
+		String user = "root";
+		String password = "root";
+		try{
+			System.out.println("Connection to database " + url);
+			Connection connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Connection succesful");			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
